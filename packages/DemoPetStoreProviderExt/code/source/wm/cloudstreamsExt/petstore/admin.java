976ad060b7a877b595113c6a2945f286c952f10f
Sandeep <sandeep.lati@gmail.com>
2020-12-09 14:51:17 +0530

    Petstore template changes

---
+++ packages/DemoPetStoreProviderExt/code/source/wm/cloudstreamsExt/petstore/admin.java
@@ -0,0 +1,50 @@
+package wm.cloudstreamsExt.petstore;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class admin
+
+{
+	// ---( internal utility methods )---
+
+	final static admin _instance = new admin();
+
+	static admin _newInstance() { return new admin(); }
+
+	static admin _cast(Object o) { return (admin)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void shutdown (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(shutdown)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+
+
+
+	public static final void startup (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(startup)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
---
+++ packages/DemoPetStoreProviderExt/code/source/wm/cloudstreamsExt/petstore/handler.java
@@ -0,0 +1,38 @@
+package wm.cloudstreamsExt.petstore;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class handler
+
+{
+	// ---( internal utility methods )---
+
+	final static handler _instance = new handler();
+
+	static handler _newInstance() { return new handler(); }
+
+	static handler _cast(Object o) { return (handler)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void interactionCallback (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(interactionCallback)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
